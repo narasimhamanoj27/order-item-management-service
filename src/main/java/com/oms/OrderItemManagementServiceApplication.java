3bfca1525655c1a6d86728725ef9c8347c738634
Manoj Garimella <narasimhamanoj27@gmail.com>
2020-07-07 00:41:31 +0530

    Initial commit

---
+++ src/main/java/com/oms/OrderItemManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderItemManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderItemManagementServiceApplication.class, args);
+	}
+
+}
